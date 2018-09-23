aa31114596f012626f1d8a3093b321be4c874b2a
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2018-09-24 00:55:30 +0900

    add contract of token

---
+++ dapps-token/contracts/DappsToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+contract DappsToken is StandardToken {
+    string public name = "DappsToken";
+    string public symbol = "DTKN";
+    uint public decimals = 18;
+
+    constructor(uint initialSupply) public {
+        totalSupply_ initialSupply;
+        balances[msg.sender] = initialSupply;
+    }
+}
