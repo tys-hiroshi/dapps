7a8af87d2bf83360472d0852b6c91a3b283d05b3
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2018-09-26 22:22:02 +0900

    mod

--- dapps-token/contracts/DappsToken.sol
+++ dapps-token/contracts/DappsToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
-
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
 contract DappsToken is StandardToken {
     string public name = "DappsToken";
     string public symbol = "DTKN";
