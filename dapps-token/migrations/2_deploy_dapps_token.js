819b93eb53598f260b1da6bb943d29b27d1c7af2
Hiroshi Tashiro <43472251+tys-hiroshi@users.noreply.github.com>
2018-10-07 14:47:12 +0900

    write gas

--- dapps-token/migrations/2_deploy_dapps_token.js
+++ dapps-token/migrations/2_deploy_dapps_token.js
@@ -1,6 +1,8 @@
 var DappsToken = artifacts.require("./DappsToken.sol");
 
 module.exports = function(deployer){
-    var initialSupply = 1000;
-    deployer.deploy(DappsToken, initialSupply);
+    var initialSupply = 1000e18;
+    deployer.deploy(DappsToken, initialSupply, {
+        gas: 2000000
+    });
 }
