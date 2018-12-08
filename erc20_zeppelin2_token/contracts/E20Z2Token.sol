6719aa2191093c399b664530304f764e0196d4f6
Hiroshi Tashiro <tashiro.hiroshi@toyoko-sys.co.jp>
2018-12-09 02:20:06 +0900

    test, migrate

--- erc20_zeppelin2_token/contracts/E20Z2Token.sol
+++ erc20_zeppelin2_token/contracts/E20Z2Token.sol
@@ -1,25 +1,25 @@
 pragma solidity ^0.4.23;
 
-//import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 //import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract E20Z2Token is ERC20Mintable, ERC20Burnable, ERC20Detailed {
+contract E20Z2Token is ERC20, ERC20Mintable, ERC20Burnable, ERC20Detailed {
 
     string private _name = "E20Z2Token";
     string private _symbol = "Z2T";
     uint8 private _decimals = 18;
 
-    uint value = 1000000;
+    address account = msg.sender;
 
-    constructor()
+    constructor(uint value)
         ERC20Detailed(_name, _symbol, _decimals)
         ERC20Burnable()
         ERC20Mintable()
         public
     {
-        _mint(msg.sender, value);
+        _mint(account, value);
     }
 }
