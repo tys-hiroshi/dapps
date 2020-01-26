2681b50901506b49bb2064758c9764b8781a9edb
tys-hiroshi <th4inf@gmail.com>
2020-01-26 15:51:44 +0900

    complete tutorial

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.5.0;
 
 contract Adoption {
 
-    address[16] public adopters;  //This is an array of Ethereum addresses. 
+    address[16] public adopters;  //This is an array of Ethereum addresses.   16個のアドレスを用意する(初期値: 0x0000000000000000000000000000000000000000)
 
     // Adopting a pet
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);
 
-        adopters[petId] = msg.sender;
+        adopters[petId] = msg.sender;  //「msg.sender」を使うと、関数を呼び出したスマートコントラクトのアドレスを参照できる
 
         return petId;
     }
