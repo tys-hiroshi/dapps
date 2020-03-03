181db620110bb27a1a7956cd5977d172db633a6b
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-03-03 23:02:59 +0900

    bottle web

---
+++ bottle_web/hello.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+from bottle import route, run
+
+
+@route('/hello')
+def hello():
+    return "Hello World!"
+
+
+run(host='localhost', port=8080, debug=True)
+
+# http://localhost:8080/hello
