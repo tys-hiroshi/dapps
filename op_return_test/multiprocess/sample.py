fe55373b3f9e2131f539297f7dfa8c42d6c268e5
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-03-25 20:06:37 +0900

    add

---
+++ op_return_test/multiprocess/sample.py
@@ -0,0 +1,13 @@
+import time
+from joblib import Parallel, delayed
+from multiprocessing import Pool
+import multiprocessing as multi
+from more_itertools import flatten
+import sys
+import functools
+
+# python3 -m venv .venv
+# source .venv/bin/activate
+
+# https://qiita.com/yukiB/items/203a6248c2d466b80d38
+
