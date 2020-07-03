a50a0dabe3d834a2926d1af6829817dfa7db8979
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-07-03 23:11:45 +0900

    2倍算の計算
    https://www.citp-forum.ipsj.or.jp/wp-content/uploads/2018/09/CITP_report2017_05_akane.pdf

--- eciespy_test/README.md
+++ eciespy_test/README.md
@@ -3,6 +3,7 @@ https://pypi.org/project/eciespy/
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install eciespy
+pip3 install ecdsa
 pip3 freeze | grep -v "pkg-resources" > requirements.txt
 
 https://qiita.com/unhurried/items/dc4734b8cc6b9c7493da
@@ -15,4 +16,7 @@ Elliptic Curve Digital Signature Algorithm (ECDSA)
 暗号化
 Elliptic Curve Integrated Encryption Scheme (ECIES)
 
+https://www.citp-forum.ipsj.or.jp/wp-content/uploads/2018/09/CITP_report2017_05_akane.pdf
 
+
+pip3 install ecdsa 
--- eciespy_test/ellipse_graph.py
+++ eciespy_test/ellipse_graph.py
@@ -196,4 +196,49 @@ def plot_ec(a, b, p):
     plt.plot(xlist, ylist, point_style , color="black")
     plt.show()
 #plot_ec(1, 1, 17)
-plot_ec(2, 17, 31)
+#plot_ec(2, 17, 31)
+
+# F_p 上の y^2=x^3+ax+b での p, a, b
+p, a, b = 23, 1, 1
+G=(0,1)
+def inv(n, p):
+    return pow(n, p-2, p)
+
+def ec_double(A):
+    l = (((3 * A[0] **2) + a) * inv(2*A[1], p)) % p
+    x = (l ** 2 - A[0] - A[0]) % p
+    y = (l * (A[0] - x) - A[1]) % p
+    return x, y
+
+G2=ec_double(G)
+print(G2) # (6, 19)
+
+def ec_add(A, B):
+    l = ((B[1] - A[1]) * inv(B[0] - A[0], p)) % p
+    x = (l ** 2 - B[0] - A[0]) % p
+    y = (l * (A[0] - x) - A[1]) % p
+    return x, y
+G3 = ec_add(G2, G)
+print(G3) # (3, 13)
+
+from ecdsa.ellipticcurve import CurveFp, Point
+
+# F_p 上の y^2=x^3+ax+b の意味での p, a, b
+p, a, b = 23, 1, 1
+c = CurveFp(p,a,b)
+# G を c 上の点(0,1)とする
+G = Point(c, 0, 1)
+current = G
+for i in range(1,28):
+    print("{}G:¥t{}".format(i,current))
+    current = current + G
+
+# F_p 上の y^2=x^3+ax+b の意味での p, a, b
+p, a, b = 31, 2, 17
+c = CurveFp(p,a,b)
+# G を c 上の点(10,13)とする
+G = Point(c, 10, 13)
+current = G
+for i in range(1,35):
+    print("{}G:¥t{}".format(i,current))
+    current = current + G
